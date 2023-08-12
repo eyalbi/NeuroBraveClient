1ffd0eba7b4de428256cbe52a0360b552e4c187d
eyalchecknet <130969117+eyalchecknet@users.noreply.github.com>
2023-08-13 00:35:09 +0300

    add docs

--- client.py
+++ client.py
@@ -9,12 +9,16 @@ import webbrowser
 TOKEN = str(uuid.uuid4())
 # ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 # ssl_context.load_verify_locations('server.cert')  # Provide the path to the server's certificate
-
+#create socket object
 sio = socketio.AsyncClient()
+
+#get client generate period and send period for each data
 generate_period = float(input("Enter mouse position generate_period (seconds): "))
 send_period = float(input("Enter mouse position send_period (seconds): "))
 generate_period_2 = float(input("Enter CPU load generate_period (seconds): "))
 send_period_2 = float(input("Enter CPU load send_period (seconds): "))
+
+#send mouse data over socket
 async def send_data_mouse():
     buffer_mouse = []
     while True:
@@ -25,6 +29,7 @@ async def send_data_mouse():
             await sio.emit('send_data', {'type': 'mouse', 'data': buffer_mouse, 'token': TOKEN})
             buffer_mouse.clear()
 
+#send cpu data over socket
 async def send_data_cpu():
     buffer_cpu = []
     while True:
@@ -35,14 +40,21 @@ async def send_data_cpu():
             await sio.emit('send_data', {'type': 'cpu', 'data': buffer_cpu, 'token': TOKEN})
             buffer_cpu.clear()
 
-async def main():
 
+async def main():
+    '''
+    1.connect to socket
+    2.login socket to a specific room
+    3.run asyncio eventloop with two function that sends the data
+    '''
     await sio.connect('wss://neuro-brave-server-b222fd4c7de9.herokuapp.com')
     await sio.emit('join_room', {'room': TOKEN})
     # You can run both in an asyncio.gather() to make them concurrent.
     await asyncio.gather(send_data_mouse(), send_data_cpu())
 
+#open webrowser with server that deployed on heroku for your conviniece
 webbrowser.open(url=f"https://neuro-brave-server-b222fd4c7de9.herokuapp.com/?token={TOKEN}")
+
 # Run the main coroutine using asyncio
 asyncio.run(main())
 
